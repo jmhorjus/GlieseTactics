f9818c9675dc9ce4473b81b3362ed986722d8b86
jmhorjus <jancomx@yahoo.com>
2015-12-10 12:35:48 -0500

    Final publish and install update for CS664 version!

--- GlieseTactics/GlieseTacticsCode/Properties/AssemblyInfo.cs
+++ GlieseTactics/GlieseTacticsCode/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jan's CS664 Project")]
+[assembly: AssemblyTitle("Gliese Tactics (CS664 Version)")]
 [assembly: AssemblyProduct("Gliese Tactics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("Jan Horjus")]
